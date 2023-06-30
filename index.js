2cb6dd6ade647345fffcaaf3ebb0572feb402f17
anasalmezyd <Anas.almziad@gmail.com>
2023-06-30 21:37:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+//adding fun
+function add(a, b) {
+    return a + b;
+}
+//subtract fun
+function subtract(a, b){
+     return a - b;
+}
+//multiply fun
+function multiply(a, b){
+    return a * b;
+}
+//divide fun
+function divide(a, b) {
+    return a / b;
+}
+//increment fun
+function increment(n) {
+    return ++n;
+}
+// decrement func
+function decrement(n) {
+    return --n;
+}
+//makeInt func
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+// Implement preserveDecimal fun
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
